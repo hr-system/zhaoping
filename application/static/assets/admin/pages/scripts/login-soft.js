168f11922ea49f836585358c13ab6d1000b07063
liustenfan <49465564+liustenfan@users.noreply.github.com>
2019-06-25 15:31:38 +0800

    登录国籍图标

--- application/static/assets/admin/pages/scripts/login-soft.js
+++ application/static/assets/admin/pages/scripts/login-soft.js
@@ -126,7 +126,7 @@ var Login = function () {
 
 		function format(state) {
             if (!state.id) return state.text; // optgroup
-            return "<img class='flag' src='../../assets/global/img/flags/" + state.id.toLowerCase() + ".png'/>&nbsp;&nbsp;" + state.text;
+            return "<img class='flag' src='/static/assets/global/img/flags/" + state.id.toLowerCase() + ".png'/>&nbsp;&nbsp;" + state.text;
         }
 
 
